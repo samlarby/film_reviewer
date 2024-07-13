e5a579e6fd02cd6917864f726fc6290aae182210
Sam Larby <samlarby1997@hotmail.com>
2024-07-13 14:35:18 +0000

    add login page

--- app.py
+++ app.py
@@ -46,6 +46,31 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            # ensure the password matches
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # if the password does not match then display a message
+                flash("Incorrect password or username was entered")
+                return redirect(url_for("login"))
+
+        else:
+            # if the username does not exist display message
+            flash("Incorrect password or username was entered")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
