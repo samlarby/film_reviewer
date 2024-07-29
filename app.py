362b4cc41308df4af99760cc40a54123ecb80a54
Sam Larby <samlarby1997@hotmail.com>
2024-07-29 19:02:13 +0000

    making edits to make it pep 8 compliant

--- app.py
+++ app.py
@@ -20,9 +20,6 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-
-
-
 @app.route("/")
 @app.route("/films")
 def films():
@@ -33,15 +30,19 @@ def films():
 
     query = request.args.get('query', '')
     if query:
-        films = list(films_collection.find({"film_name": {"$regex": query, "$options": "i"}}).skip(offset).limit(per_page))
-        total = films_collection.count_documents({"film_name": {"$regex": query, "$options": "i"}})
+        films = list(films_collection.find(
+            {"film_name": {"$regex": query, "$options": "i"}}).skip(
+                offset).limit(per_page))
+        total = films_collection.count_documents(
+            {"film_name": {"$regex": query, "$options": "i"}})
     else:
         films = list(films_collection.find().skip(offset).limit(per_page))
         total = films_collection.count_documents({})
 
     pagination = Pagination(page=page, total=total, per_page=per_page)
 
-    return render_template("films.html", films=films, pagination=pagination, query=query)
+    return render_template(
+        "films.html", films=films, pagination=pagination, query=query)
 
 
 @app.route("/search", methods=["POST"])
@@ -85,15 +86,14 @@ def login():
         # check if username already exists
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-        
         if existing_user:
             # ensure the password matches
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
-                    return redirect(
-                        url_for("films", username=session["user"]))
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(
+                    url_for("films", username=session["user"]))
             else:
                 # if the password does not match then display a message
                 flash("Incorrect password or username was entered")
@@ -133,7 +133,7 @@ def add_film():
 @app.route("/edit_film/<film_id>", methods=["GET", "POST"])
 def edit_film(film_id):
     if request.method == "POST":
-        submit = { "$set": {
+        submit = {"$set": {
             "username": session.get("user"),
             "film_name": request.form.get("film_name"),
             "director": request.form.get("director"),
@@ -143,7 +143,7 @@ def edit_film(film_id):
         mongo.db.films.update_one({"_id": ObjectId(film_id)}, submit)
         flash("Film successfully edited")
         return redirect(url_for('films'))
-    film = mongo.db.films.find_one({"_id":ObjectId(film_id)})
+    film = mongo.db.films.find_one({"_id": ObjectId(film_id)})
     username = session.get("user")
     return render_template("edit_film.html", film=film, username=username)
 
@@ -174,6 +174,7 @@ def add_review(film_id):
     username = session.get("user")
     return render_template("add_review.html", film=film, username=username)
 
+
 @app.route("/edit_review/<film_id>/<review_id>", methods=["GET", "POST"])
 def edit_review(film_id, review_id):
     if request.method == "POST":
@@ -188,11 +189,12 @@ def edit_review(film_id, review_id):
         )
         flash("Review successfully updated")
         return redirect(url_for('films', film_id=film_id))
-    film = mongo.db.films.find_one({"_id": ObjectId(film_id), "reviews.review_id": review_id},
-                                    {"reviews.$": 1})
+    film = mongo.db.films.find_one(
+        {"_id": ObjectId(film_id), "reviews.review_id": review_id},
+        {"reviews.$": 1})
     review = film["reviews"][0]
     return render_template("edit_review.html", film_id=film_id, review=review)
-    
+
 
 @app.route("/delete_review/<film_id>/<review_id>")
 def delete_review(film_id, review_id):
@@ -203,7 +205,8 @@ def delete_review(film_id, review_id):
     flash("Review successfully deleted")
     return redirect(url_for("films"))
 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT")),
-    debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
